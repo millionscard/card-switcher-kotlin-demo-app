c2a5279c89462ea014e0221470b82794614da3ae
Nikunjp1904 <nikunjp1904@gmail.com>
2022-09-28 13:24:07 +0530

    Implement user register and session create API. Update lib. Implement subscription canceller flow.

---
+++ app/src/main/java/com/knotapi/demo/CreateUserRequest.kt
@@ -0,0 +1,14 @@
+package com.knotapi.demo
+
+class CreateUserRequest {
+    var first_name: String? = null
+    var last_name: String? = null
+    var email: String? = null
+    var phone_number: String? = null
+    var password: String? = null
+    var address1: String? = null
+    var address2: String? = null
+    var state: String? = null
+    var city: String? = null
+    var postal_code: String? = null
+}
--- app/src/main/java/com/knotapi/demo/MainActivity.kt
+++ app/src/main/java/com/knotapi/demo/MainActivity.kt
@@ -1,37 +1,124 @@
 package com.knotapi.demo
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.knotapi.cardonfileswitcher.CardOnFileSwitcher
+import com.knotapi.cardonfileswitcher.Environment
 import com.knotapi.cardonfileswitcher.OnSessionEventListener
+import com.knotapi.cardonfileswitcher.SubscriptionCanceler
 import com.knotapi.cardonfileswitcher.model.Customization
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity(), OnSessionEventListener {
-    var cardOnFileSwitcher: CardOnFileSwitcher? = null
-    var button: Button? = null
+
+    private var progressDialog: ProgressDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //
-        button = findViewById(R.id.button)
-        button?.setOnClickListener(View.OnClickListener { view: View? ->
-            val customization = Customization()
-            customization.textColor = "#fff000"
-            customization.primaryColor = "#ff0000"
-            customization.companyName = "Facebook"
-            cardOnFileSwitcher = CardOnFileSwitcher.getInstance()
-            cardOnFileSwitcher?.init(this, this, customization)
-            cardOnFileSwitcher?.openCardOnFileSwitcher(
-                "fc3fe2a1-0795-4e33-84c6-5f34ab96f61a",
-                intArrayOf(1, 3),
-                false
-            )
+
+        progressDialog = ProgressDialog(this)
+        progressDialog?.setMessage("Please wait...")
+        progressDialog?.setCancelable(false)
+
+        btnOpenCardSwitcher.setOnClickListener {
+            callCreateUserAPI(false)
+        }
+
+        btnOpenSubscriptionCanceller.setOnClickListener {
+            callCreateUserAPI(true)
+        }
+    }
+
+    private fun callCreateUserAPI(openSubCanceller: Boolean) {
+        val createUserRequest = CreateUserRequest()
+        createUserRequest.first_name = "Nikunj"
+        createUserRequest.last_name = "Patel"
+        createUserRequest.email = "production@knotapi.com"
+        createUserRequest.phone_number = "+18024687679"
+        createUserRequest.password = "password"
+        createUserRequest.address1 = "348 W 57th St"
+        createUserRequest.address2 = "#367"
+        createUserRequest.state = "NY"
+        createUserRequest.city = "New York"
+        createUserRequest.postal_code = "10019"
+
+        progressDialog?.show()
+        val call: Call<CreateUserResponse>? =
+            RetrofitClient.instance?.myApi?.createUserAPI(createUserRequest)
+        call?.enqueue(object : Callback<CreateUserResponse?> {
+            override fun onResponse(
+                call: Call<CreateUserResponse?>,
+                response: Response<CreateUserResponse?>
+            ) {
+                val createUserResponse = response.body()
+                callCreateSessionAPI(createUserResponse?.token, openSubCanceller)
+            }
+
+            override fun onFailure(call: Call<CreateUserResponse?>, t: Throwable) {
+                progressDialog?.hide()
+                Toast.makeText(applicationContext, "An error has occurred", Toast.LENGTH_LONG)
+                    .show()
+            }
         })
+    }
+
+    private fun callCreateSessionAPI(token: String?, openSubCanceller: Boolean) {
+        val call: Call<CreateSessionResponse>? =
+            RetrofitClient.instance?.myApi?.createSessionAPI("Bearer $token")
+        call?.enqueue(object : Callback<CreateSessionResponse?> {
+            override fun onResponse(
+                call: Call<CreateSessionResponse?>,
+                response: Response<CreateSessionResponse?>
+            ) {
+                val createSessionResponse = response.body()
+                progressDialog?.hide()
+                if (openSubCanceller) {
+                    openSubscriptionCanceller(createSessionResponse?.session)
+                } else {
+                    openCardSwitcher(createSessionResponse?.session)
+                }
+            }
+
+            override fun onFailure(call: Call<CreateSessionResponse?>, t: Throwable) {
+                progressDialog?.hide()
+                Toast.makeText(applicationContext, "An error has occurred", Toast.LENGTH_LONG)
+                    .show()
+            }
+        })
+    }
+
+    private fun openCardSwitcher(sessionID: String?) {
+        val customization = Customization()
+        customization.textColor = "#ffffff"
+        customization.primaryColor = "#000000"
+        customization.companyName = "Millions"
+
+        val cardOnFileSwitcher = CardOnFileSwitcher.getInstance()
+        cardOnFileSwitcher.init(this, "ab86955e-22f4-49c3-97d7-369973f4cb9e", Environment.SANDBOX)
+        cardOnFileSwitcher.setCustomization(customization)
+        cardOnFileSwitcher.onSessionEventListener = this
+        cardOnFileSwitcher.openCardOnFileSwitcher(sessionID, intArrayOf())
+    }
+
+    private fun openSubscriptionCanceller(sessionID: String?) {
+        val customization = Customization()
+        customization.textColor = "#fff000"
+        customization.primaryColor = "#ff0000"
+        customization.companyName = "Millions"
+
+        val subscriptionCanceler = SubscriptionCanceler.getInstance()
+        subscriptionCanceler.setCustomization(customization)
+        subscriptionCanceler.setOnSessionEventListener(this)
+        subscriptionCanceler.init(this, "ab86955e-22f4-49c3-97d7-369973f4cb9e", Environment.SANDBOX)
+        subscriptionCanceler.openCardOnFileSwitcher(sessionID)
     }
 
     override fun onInvalidSession(sessionId: String?, errorMessage: String?) {
